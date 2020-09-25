f7efac160d1d7181aad885129f9ecd951bbab6fb
adadesions <adadesions@gmail.com>
2020-09-25 10:45:39 +0700

    Implement router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React from 'react';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 // Developer Import
 import TodoContainer from './components/Todo/TodoContainer'
 import NavBar from './components/NavBar/NavBar'
 
+const PrintApp = () => {
+  return (
+    <h1>Print App Page</h1>
+  )
+}
+
+const Profile = ({ username }) => {
+  return (
+    <div className="profile">
+      <h1>Profile Page</h1>
+      <h3>logged in by {username}</h3>
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div className="app">
-      <NavBar />
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col">
-            <TodoContainer />
-          </div>
-          <div className="col">
-            <TodoContainer style={{marginTop: '5em'}}/>
-          </div>
-          <div className="col">
-            <TodoContainer />
+    <Router>
+      <div className="app">
+        <NavBar />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <TodoContainer />
+            </div>
+            <div className="col">
+              <Switch>
+                <Route path="/printapp">
+                  <PrintApp />
+                </Route>
+                <Route path="/profile">
+                  <Profile />
+                </Route>
+                <Route path="/">
+                  <TodoContainer />
+                </Route>
+              </Switch>
+            </div>
+            <div className="col">
+              <TodoContainer />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
